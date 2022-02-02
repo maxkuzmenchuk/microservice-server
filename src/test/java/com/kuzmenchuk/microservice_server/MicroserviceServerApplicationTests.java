244fd96a5de78768433a6f3d04b1b562e3131ca7
Maxim Kuzmenchuk <46691880+maxkuzmenchuk@users.noreply.github.com>
2022-02-02 14:34:46 +0300

    eureka server and microservice api gateway

---
+++ src/test/java/com/kuzmenchuk/microservice_server/MicroserviceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuzmenchuk.microservice_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
